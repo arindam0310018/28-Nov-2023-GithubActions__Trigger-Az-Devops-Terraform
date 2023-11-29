1777210263f728786411d11ac82d75b8b601a7de
Arindam Mitra <mail2arindam2003@yahoo.com>
2023-11-29 21:49:03 +0100

    Update az-resource-provider-registration.tf

--- az-resource-provider-registration.tf
+++ az-resource-provider-registration.tf
@@ -2,14 +2,9 @@
 ## Resource Provider Registration:-
 #####################################
 
-# resource "azurerm_resource_provider_registration" "az-resource-provider-register" {
-#   count	  = length(var.az-resource-provider-names)
-#   name    = var.az-resource-provider-names[count.index] #"Microsoft.PolicyInsights"
-# }
-
 resource "azurerm_resource_provider_registration" "az-resource-provider-register" {
   count   = var.create-az-resource-provider-names == true ? length(var.az-resource-provider-names) : 0
-  name    = var.az-resource-provider-names[count.index] #"Microsoft.PolicyInsights"
+  name    = var.az-resource-provider-names[count.index]
 }
 
 
